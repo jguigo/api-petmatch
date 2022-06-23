119c8959cf0c3288c33e3bad2dfaecdd82fdd42d
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-23 01:25:55 -0300

    import routes and middleware handleError to app

--- src/shared/infra/http/app.js
+++ src/shared/infra/http/app.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('../database');
+const express = require("express");
+const cors = require("cors");
+const db = require("../database");
+const routes = require("./routes/index");
+const handleError = require("../../middleware/handleError");
 
 db.hasConnection();
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
+
+app.use(routes);
+
+app.use(handleError);
 
 module.exports = app;
