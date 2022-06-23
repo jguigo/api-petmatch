819207ae265e17da5e60f80fd45a0cc1bc26a244
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-23 01:25:18 -0300

    feat: create routes

---
+++ src/shared/infra/http/routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const userRoutes = require("./userRoutes");
+const petsRoutes = require("./petsRoutes");
+const { authController } = require("../../../../domain/auth/controller/index");
+const loginValidation = require("../../../../domain/auth/validation/index");
+
+const routes = express.Router();
+
+routes.post("/login", loginValidation.login, authController.auth);
+
+routes.use(userRoutes);
+routes.use(petsRoutes);
+
+module.exports = routes;
