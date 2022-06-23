69b99ac19172c699593ea890d7429ef3bf0f86bb
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-23 17:25:00 -0300

    feat: created statusresponse test and bodyrespose test for users

--- src/domain/users/controllers/userController.spec.js
+++ src/domain/users/controllers/userController.spec.js
@@ -3,8 +3,32 @@ const app = require('../../../shared/infra/http/app');
 
 describe("Listar todos os users", () => {
     test("Em caso de sucesso, retornar o status 200", async () => {
-      const expectResponse = await supertest(app)
+      const StatusEsperado = await supertest(app)
         .get('/users')
-      expect(expectResponse.status).toBe(200);
+      expect(StatusEsperado.status).toBe(200);
     });
   });
+  describe("Listar users por id", () => {
+    test("Em caso de sucesso, retornar o status 200", async () => {
+      const StatusEsperado = await supertest(app)
+        .get('/users/1')
+      expect(StatusEsperado.status).toBe(200);
+    });
+  });
+  describe("Dados recebidos", () => {
+    test("Em caso de sucesso, deve retornar os atributos desejados", async () => {
+      const BodyEsperado = await supertest(app)
+        .get('/users/1')
+        expect(BodyEsperado.body).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                email: expect.any(String),
+                cidade: expect.any(String),
+                uf: expect.any(String),
+            }),
+          );
+    });
+  });
+
+  
