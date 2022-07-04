9cd37318b1f8409c7bdea52cab998e846d09f413
frederic rodrigues <fredericlsr@gmail.com>
2022-07-04 14:36:39 -0300

    adicionado config app.use(express)

--- src/shared/infra/http/app.js
+++ src/shared/infra/http/app.js
@@ -4,11 +4,16 @@ const db = require("../database");
 const routes = require("./routes/index");
 const handleError = require("../../middleware/handleError");
 
+
 db.hasConnection();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(src/public));
 
 app.use(cors());
 
