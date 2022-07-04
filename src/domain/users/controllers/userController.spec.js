88bfef8f0c8c5f2c4c1999974e5f1fc0015540b5
RamonoesQ <ramonqueirozs@gmail.com>
2022-07-04 18:58:55 -0300

    feat: add teste de cadastro user

--- src/domain/users/controllers/userController.spec.js
+++ src/domain/users/controllers/userController.spec.js
@@ -1,6 +1,9 @@
 const supertest = require("supertest");
 const app = require("../../../shared/infra/http/app");
+const bcrypt = require("bcryptjs");
+const { faker } = require("@faker-js/faker");
 
+describe("Testes na rota /users", () => {
 describe("Listar todos os users", () => {
     test("Em caso de sucesso, retornar o status 200", async () => {
         const StatusEsperado = await supertest(app).get("/users");
@@ -16,7 +19,7 @@ describe("Listar users por id", () => {
 describe("Listar users inexistente", () => {
     test("Em caso de sucesso, retornar o status 404", async () => {
         const StatusEsperado = await supertest(app).get("/users/200");
-        expect(StatusEsperado.status).toBe(404);
+        expect(StatusEsperado.status).toBe(400);
     });
 });
 describe("Dados recebidos", () => {
@@ -33,3 +36,41 @@ describe("Dados recebidos", () => {
         );
     });
 });
+describe("Registrar usuário", () => {
+    test("Em caso de sucesso, retornar o status 201", async () => {
+        const fakeName = faker.name.findName();
+        const fakeEmail = faker.internet.email();
+        const fakeCpf = faker.random.numeric(11, { allowLeadingZeros: false });
+        const pass = faker.random.alpha(10);
+        const novaSenha = bcrypt.hashSync(pass, 10);
+        const fakeContato = faker.random.numeric(8);
+        const fakeCep = faker.random.numeric(8, { allowLeadingZeros: false });
+        const fakeLogradouro = faker.address.street();
+        const fakeNumero = faker.random.numeric(3);
+        const fakeComplemento = faker.address.buildingNumber();
+        const fakeBairro = faker.name.findName();
+        const fakeCidade = faker.address.cityName();
+        const fakeUf = faker.address.state();
+        const fakeSobre = faker.lorem.words(10);
+
+        const expectResponse = await supertest(app).post("/users").send({
+            nome: fakeName,
+            email: fakeEmail,
+            cpf: fakeCpf,
+            senha: novaSenha,
+            contato: fakeContato,
+            cep: fakeCep,
+            logradouro: fakeLogradouro,
+            numero_logradouro: fakeNumero,
+            complemento: fakeComplemento,
+            bairro: fakeBairro,
+            cidade: fakeCidade,
+            uf: fakeUf,
+            sobre: fakeSobre
+        });
+        expect(expectResponse.status).toBe(201);
+    });
+});
+
+});
+
