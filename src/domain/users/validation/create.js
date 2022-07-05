b70564abca967f15f686197fe6e71602fbf5454e
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-05 20:15:14 -0300

    refeact: user validation

--- src/domain/users/validation/create.js
+++ src/domain/users/validation/create.js
@@ -3,17 +3,8 @@ const { validate, Joi } = require("express-validation");
 module.exports = validate({
     body: Joi.object({
         nome: Joi.string().required(),
+        sobrenome: Joi.string().required(),
         email: Joi.string().email().required(),
-        cpf: Joi.string(),
         senha: Joi.string().required(),
-        contato: Joi.string(),
-        cep: Joi.string(),
-        logradouro: Joi.string(),
-        numero_logradouro: Joi.string(),
-        complemento: Joi.string(),
-        bairro: Joi.string(),
-        cidade: Joi.string(),
-        uf: Joi.string(),
-        sobre: Joi.string(),
     }),
 });
