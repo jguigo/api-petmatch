21df66ff5a3a31f63eecc04e2a3ef36391bf6159
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-06 19:08:56 -0300

    adding upload image on create pet

--- src/domain/pets/useCase/CreatePetUseCase.js
+++ src/domain/pets/useCase/CreatePetUseCase.js
@@ -1,25 +1,25 @@
 const { petRepository } = require("../../repository/index");
 const { uploads } = require("../../../config/cloudinary");
+const fs = require("fs");
 
 class CreatePetUseCase {
     async create(data) {
         const objPet = data.body;
-        const uploadedImage = data.files[0];
+        const uploadedImage = data.file;
 
-        const linkImage = await uploads(uploadedImage, petsImage);
+        const linkImage = await uploads(uploadedImage.path, "petsImage");
 
-        console.log(linkImage);
+        fs.unlinkSync(uploadedImage.path);
 
         Object.assign(objPet, {
             userID: data.auth.id,
-            petImage: linkImage.imageUrl
+            petImage: linkImage.imageUrl,
         });
 
         const newPet = await petRepository.create(objPet);
 
         return newPet;
     }
-    
 }
 
 module.exports = CreatePetUseCase;
