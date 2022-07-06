c2e74f5ef231d8298b7b7505c313cbcf13bdf784
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-06 19:09:46 -0300

    removing required from body

--- src/domain/pets/validation/update.js
+++ src/domain/pets/validation/update.js
@@ -2,23 +2,29 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
     params: Joi.object({
-        id: Joi.number().required()
+        id: Joi.number().required(),
     }),
-    
+
     body: Joi.object({
-        especie: Joi.string().required(),
-        nome: Joi.string().required(),
-        idade: Joi.number().required(),
-        sexo: Joi.string().required(),
-        peso: Joi.number().required(),
-        tamanho: Joi.number().required(),
-        porte: Joi.number().required(),
-        cor: Joi.string().required(),
-        raca: Joi.string().required(),
-        castrado: Joi.boolean().required(),
-        vacinado: Joi.boolean().required(),
-        chip: Joi.number().required(),
-        sobre: Joi.string().required(),
-        petImage: Joi.string().required(),
-    })
-})
+        especie: Joi.string().valid(
+            "Cachorro",
+            "Gato",
+            "Ave",
+            "Réptil",
+            "Peixe",
+        ),
+        nome: Joi.string(),
+        idade: Joi.number(),
+        sexo: Joi.string().valid("Macho", "Fêmea"),
+        peso: Joi.number(),
+        tamanho: Joi.number(),
+        porte: Joi.string().valid("Pequeno", "Médio", "Grande"),
+        cor: Joi.string().valid("Escuro", "Claro", "Misto", "Caramelo"),
+        raca: Joi.string(),
+        castrado: Joi.boolean(),
+        vacinado: Joi.boolean(),
+        chip: Joi.string(),
+        sobre: Joi.string(),
+        petImage: Joi.any(),
+    }),
+});
