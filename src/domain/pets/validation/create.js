a3ad4126bf805897d3f0068cffc621b114e0a5f1
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-06 17:43:21 -0300

    adding valid on enum fields

--- src/domain/pets/validation/create.js
+++ src/domain/pets/validation/create.js
@@ -2,19 +2,23 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
     body: Joi.object({
-        especie: Joi.string().required(),
+        especie: Joi.string()
+            .valid("Cachorro", "Gato", "Ave", "Réptil", "Peixe")
+            .required(),
         nome: Joi.string().required(),
         idade: Joi.number().required(),
-        sexo: Joi.string().required(),
+        sexo: Joi.string().valid("Macho", "Fêmea").required(),
         peso: Joi.number().required(),
         tamanho: Joi.number().required(),
-        porte: Joi.number().required(),
-        cor: Joi.string().required(),
+        porte: Joi.string().valid("Pequeno", "Médio", "Grande").required(),
+        cor: Joi.string()
+            .valid("Escuro", "Claro", "Misto", "Caramelo")
+            .required(),
         raca: Joi.string().required(),
         castrado: Joi.boolean().required(),
         vacinado: Joi.boolean().required(),
         chip: Joi.string(),
         sobre: Joi.string().required(),
-        petImage: Joi.string().required(),
-    })
-})
+        petImage: Joi.any().required(),
+    }),
+});
