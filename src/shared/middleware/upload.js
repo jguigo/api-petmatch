b9715beeb34eec2882f8ad7815d453514db200e6
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-06 18:46:03 -0300

    fix: update file size

--- src/shared/middleware/upload.js
+++ src/shared/middleware/upload.js
@@ -3,39 +3,41 @@ const path = require("path");
 const fs = require("fs");
 
 const pathImageFolder = path.join("src/public", "images");
-const maxSize = 1 * 1000 * 1000;
+const maxSize = 1 * 1024 * 1024;
 const maxFile = 3;
 
 const storage = multer.diskStorage({
-   destination: function (req, file, cb) {
-      if (!fs.existsSync(pathImageFolder)) {
-         fs.mkdirSync(pathImageFolder, { recursive: true });
-      }
+    destination: function (req, file, cb) {
+        if (!fs.existsSync(pathImageFolder)) {
+            fs.mkdirSync(pathImageFolder, { recursive: true });
+        }
 
-      cb(null, pathImageFolder);
-   },
+        cb(null, pathImageFolder);
+    },
 
-   filename: function (req, file, cb) {
-      cb(null, Date.now() + "-" + file.originalname);
-   },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
 });
 
 const fileFilter = (req, file, cb) => {
-   if((file.mimetype).includes('jpeg') || (file.mimetype).includes('png') || (file.mimetype).includes('jpg')){
-       cb(null, true);
-   } else{
-       cb(null, false);
-
-   }
+    if (
+        file.mimetype.includes("jpeg") ||
+        file.mimetype.includes("png") ||
+        file.mimetype.includes("jpg")
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 const upload = multer({
-   storage: storage,
-   fileFilter: fileFilter,
-   limits: { fieldSize: maxSize },
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: maxSize },
 });
 
-const uploadImageProfile = upload.single("imageProfile")
-const imagePet = upload.array(["petImage", maxFile])
+const imagePet = upload.single("petImage");
 
-module.exports = {uploadImageProfile, imagePet};
+module.exports = { imagePet };
