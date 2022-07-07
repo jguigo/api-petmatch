5c23ce316d3e574f66d17dc367874fa7ac47eb18
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-06 23:55:06 -0300

    adding instanceof Error

--- src/domain/adoption/controller/CreateAdoptionController.js
+++ src/domain/adoption/controller/CreateAdoptionController.js
@@ -5,6 +5,10 @@ class CreateAdoptionController {
         try {
             const newAdoption = await createAdoptionUseCase.create(req);
 
+            if (newAdoption instanceof Error) {
+                return res.status(400).json(newAdoption.message);
+            }
+
             return res.status(201).json(newAdoption);
         } catch (error) {
             return res.status(500).json(`Erro na adoção. ${error}`);
