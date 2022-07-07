abf9ccfc60178339a6a229e2b1f8dd6c4420e02e
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-07 03:23:38 -0300

    adding accept and reject adoption controller

--- src/domain/adoption/controller/index.js
+++ src/domain/adoption/controller/index.js
@@ -1,13 +1,19 @@
 const CreateAdoptionController = require("./CreateAdoptionController");
 const ChooseAdoptionController = require("./ChooseAdoptionController");
 const FindOneAdoptionController = require("./FindOneAdoptionController");
+const AcceptAdoptionApplyController = require("./AcceptAdoptionApplyController");
+const RejectAdoptionApplyController = require("./RejectAdoptionApplyController");
 
 const createAdoptionController = new CreateAdoptionController();
 const chooseAdoptionController = new ChooseAdoptionController();
 const findOneAdoptionController = new FindOneAdoptionController();
+const acceptAdoptionApplyController = new AcceptAdoptionApplyController();
+const rejectAdoptionApplyController = new RejectAdoptionApplyController();
 
 module.exports = {
     createAdoptionController,
     chooseAdoptionController,
     findOneAdoptionController,
+    acceptAdoptionApplyController,
+    rejectAdoptionApplyController,
 };
