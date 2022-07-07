f166154e03de0df930c2ebdc09e3813fa018e257
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-07 15:38:08 -0300

    adding multerError

--- src/shared/middleware/handleError.js
+++ src/shared/middleware/handleError.js
@@ -1,14 +1,24 @@
 const { ValidationError } = require("express-validation");
 const { UnauthorizedError } = require("express-jwt");
+const { MulterError } = require("multer");
+const fs = require("fs");
 
 module.exports = (error, req, res, next) => {
-  if (error instanceof ValidationError) {
-    return res.status(error.statusCode).json(error);
-  }
+    if (error instanceof ValidationError) {
+        if (req.file) {
+            fs.unlinkSync(req.file.path);
+        }
+        return res
+            .status(error.statusCode)
+            .json(`${error.message}! Preencha todos os campos corretamente...`);
+    }
 
-  if (error instanceof UnauthorizedError) {
-    return error(error.status).json(error);
-  }
+    if (error instanceof UnauthorizedError) {
+        return res.status(401).json(error.name);
+    }
 
-  return res.status(500).json(error);
+    if (error instanceof MulterError) {
+        return res.status(400).json(error.message);
+    }
+    return res.status(500).json(error);
 };
