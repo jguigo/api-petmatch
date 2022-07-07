8ec61a50f67605ee8fc68cfb6eecc0e87f669419
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-07 04:31:26 -0300

    removing choose and findOne

--- src/domain/adoption/controller/index.js
+++ src/domain/adoption/controller/index.js
@@ -1,14 +1,10 @@
 const CreateAdoptionController = require("./CreateAdoptionController");
-const ChooseAdoptionController = require("./ChooseAdoptionController");
 const AcceptAdoptionApplyController = require("./AcceptAdoptionApplyController");
 const RejectAdoptionApplyController = require("./RejectAdoptionApplyController");
-const FindOneAdoptionController = require("./FindOneAdoptionController");
 const FindAllAdoptionByOwnerController = require("./FindAllAdoptionByOwnerController");
 const FindAllAdoptionByUserController = require("./FindAllAdoptionByUserController");
 
 const createAdoptionController = new CreateAdoptionController();
-const chooseAdoptionController = new ChooseAdoptionController();
-const findOneAdoptionController = new FindOneAdoptionController();
 const acceptAdoptionApplyController = new AcceptAdoptionApplyController();
 const rejectAdoptionApplyController = new RejectAdoptionApplyController();
 const findAllAdoptionByOwnerController = new FindAllAdoptionByOwnerController();
@@ -16,8 +12,6 @@ const findAllAdoptionByUserController = new FindAllAdoptionByUserController();
 
 module.exports = {
     createAdoptionController,
-    chooseAdoptionController,
-    findOneAdoptionController,
     acceptAdoptionApplyController,
     rejectAdoptionApplyController,
     findAllAdoptionByOwnerController,
--- src/domain/adoption/useCase/index.js
+++ src/domain/adoption/useCase/index.js
@@ -1,23 +1,17 @@
 const CreateAdoptionUseCase = require("./CreateAdoptiontUseCase");
-const ChooseAdoptionUseCase = require("./ChooseAdoptionUseCase");
 const AcceptAdoptionApplyUseCase = require("./AcceptAdoptionApplyUseCase");
 const RejectAdoptionApplyUseCase = require("./RejectAdoptionApplyUseCase");
-const FindOneAdoptionUseCase = require("./FindOneAdoptionUseCase");
 const FindAllAdoptionByOwnerUseCase = require("./FindAllAdoptionByOwnerUseCase");
 const FindAllAdoptionByUserUseCase = require("./FindAllAdoptionByUserUseCase");
 
 const createAdoptionUseCase = new CreateAdoptionUseCase();
-const chooseAdoptionUseCase = new ChooseAdoptionUseCase();
 const acceptAdoptionApplyUseCase = new AcceptAdoptionApplyUseCase();
 const rejectAdoptionApplyUseCase = new RejectAdoptionApplyUseCase();
-const findOneAdoptionUseCase = new FindOneAdoptionUseCase();
 const findAllAdoptionByOwnerUseCase = new FindAllAdoptionByOwnerUseCase();
 const findAllAdoptionByUserUseCase = new FindAllAdoptionByUserUseCase();
 
 module.exports = {
     createAdoptionUseCase,
-    chooseAdoptionUseCase,
-    findOneAdoptionUseCase,
     acceptAdoptionApplyUseCase,
     rejectAdoptionApplyUseCase,
     findAllAdoptionByOwnerUseCase,
