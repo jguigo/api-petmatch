a6ddc45e4f937d3de2442b518b3778d7f73e0878
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-07 03:28:42 -0300

    adding findAll method and STATUS

--- src/domain/repository/AdoptionRepository.js
+++ src/domain/repository/AdoptionRepository.js
@@ -1,4 +1,5 @@
 const { Adoption } = require("../../shared/infra/database/models/index");
+const STATUS = require("../../shared/utils/status");
 
 class AdoptionRepository {
     async create(objAdoption) {
@@ -12,8 +13,11 @@ class AdoptionRepository {
 
     async findOne(adoptionId) {
         return await Adoption.findOne({
-            where: { id: adoptionId },
+            where: { id: adoptionId, statusAdocao: STATUS.OnApproval },
         });
+    }
+    async findAll() {
+        return await Adoption.findAll();
     }
 }
 
