5b88711fe506daa8ca9de830551e22d2099f96e1
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-07 06:02:21 -0300

    adding auth on delete route

--- src/shared/infra/http/routes/userRoutes.js
+++ src/shared/infra/http/routes/userRoutes.js
@@ -16,7 +16,12 @@ routes.get("/users", findAllUserController.findAll);
 routes.get("/users/:id", validation.findOne, findOneUserController.findOne);
 routes.post("/users/", validationUser.create, createUserController.create);
 routes.get("/users/email/:email", findByEmailUserController.findOne);
-routes.delete("/users/:id", validation.destroy, deleteUserController.findOne);
+routes.delete(
+    "/users/:id",
+    auth,
+    validation.destroy,
+    deleteUserController.findOne,
+);
 routes.put(
     "/users/:id",
     auth,
