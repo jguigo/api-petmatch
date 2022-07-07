bf12c61038308c1ad5e6b037c4d58ef17cf5f0f4
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-07 16:24:36 -0300

    fix: params name

--- src/domain/pets/validation/findByEspecie.js
+++ src/domain/pets/validation/findByEspecie.js
@@ -2,7 +2,7 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
     params: Joi.object({
-        id: Joi.string()
+        especie: Joi.string()
             .valid("Cachorro", "Gato", "Ave", "Réptil", "Peixe")
             .required(),
     }),
